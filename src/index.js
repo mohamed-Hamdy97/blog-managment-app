6d53882f46832913a2f31add4ebd916116a8a550
mohamed-aboelyazeed <42852127+mohamed-Hamdy97@users.noreply.github.com>
2025-02-14 17:27:22 +0200

    Initial app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log('the server running now on ', process.env.PORT);
+})
